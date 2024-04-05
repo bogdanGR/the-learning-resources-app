b180862d66c804de3b12372d97a74a111ccb8d51
bogdanGR <fant1k450@gmail.com>
2024-04-05 08:33:39 +0300

    dynamic componenets as tabs and adding style

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,18 @@
 <template>
     <the-header title="RememberMe"></the-header>
-    <stored-resource :resources="storedResources"></stored-resource>
+    <the-resources></the-resources>
 </template>
 
 <script>
 
-import StoredResource from './componenets/learning-resources/StoredResource.vue';
 import TheHeader from './componenets/layouts/TheHeader.vue';
+import TheResources from './componenets/learning-resources/TheResources.vue';
+
 export default {
     components: {
-        StoredResource,
-        TheHeader
+        TheHeader,
+        TheResources
     },
-    data() {
-        return {
-            storedResources: [
-                {
-                    id: 'official-guide',
-                    title: 'Official guide',
-                    description: 'The official vue.js documentation',
-                    link: 'https://vuejs.org'
-                },
-                                {
-                    id: 'google',
-                    title: 'Google',
-                    description: 'Learn to google',
-                    link: 'https://google.com'
-                }
-            ]
-        };
-    }
 }
 </script>
 
